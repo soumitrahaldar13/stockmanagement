f0adac6dad915dabe052e4469d6a239c27bf2388
sabarna basak <sabarnabasak@gmail.com>
2024-04-07 01:25:17 +0530

    move dbConnection and login action to individual folder

--- page/admin_right_side_login.php
+++ page/admin_right_side_login.php
@@ -1,25 +1,24 @@
 <?php
 session_start();
-    require_once('dbconnection.php');
-    $nam=$_SESSION["name"];
-    $query=mysqli_query($con," SELECT * FROM `login` WHERE `NAME`='$nam'")or die(mysqli_error($con));
-							// echo 1;die;
-							while($row=mysqli_fetch_array($query)){
-							$id=$row['ID'];
-							$image=$row['image'];
-							$name=$row['NAME'];
-							}
+require_once __DIR__ . '/../dbConnection/dbConnection.php';
+$nam = $_SESSION["name"];
+$query = mysqli_query($con, " SELECT * FROM `login` WHERE `NAME`='$nam'") or die(mysqli_error($con));
+// echo 1;die;
+while ($row = mysqli_fetch_array($query)) {
+  $id = $row['ID'];
+  $image = $row['image'];
+  $name = $row['NAME'];
+}
 
-    
 
-    ?>
+
+?>
 <div class="search-box">
-        <input type="text" placeholder="Search...">
-        <i class='bx bx-search' ></i>
-      </div>
+  <input type="text" placeholder="Search...">
+  <i class='bx bx-search'></i>
+</div>
 <div class="profile-details">
-        <img src="../images/admin/<?php echo $image;?>" alt="">
-        <span class="admin_name"><?php echo $name; ?></span>
-        <i class='bx bx-chevron-down' ></i>
-      </div>
-      
+  <img src="../images/admin/<?php echo $image; ?>" alt="">
+  <span class="admin_name"><?php echo $name; ?></span>
+  <i class='bx bx-chevron-down'></i>
+</div>
--- page/admin_sidenav.php
+++ page/admin_sidenav.php
@@ -1,42 +1,45 @@
 <!DOCTYPE html>
 <?php
 session_start();
-    require_once('dbconnection.php');
-    ?>
+
+require_once __DIR__ . '/../dbConnection/dbConnection.php';
+?>
 <!-- Designined by CodingLab | www.youtube.com/codinglabyt -->
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="UTF-8">
-    <title>stock management system</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <!-- Boxicons CDN Link -->
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+<head>
+  <meta charset="UTF-8">
+  <title>stock management system</title>
+  <link rel="stylesheet" href="../css/style.css">
+  <!-- Boxicons CDN Link -->
+  <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 <style type="text/css">
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  
-}
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
+  }
 
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
+  td,
+  th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
+
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
 
 
 
-.pagination ul {
+  .pagination ul {
     display: inline-block;
     *display: inline;
     margin-bottom: 0;
@@ -48,18 +51,21 @@ tr:nth-child(even) {
     -webkit-box-shadow: 0 1px 2px rgb(0 0 0 / 5%);
     -moz-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
     box-shadow: 0 1px 2px rgb(0 0 0 / 5%);
-}
-ol, ul {
+  }
+
+  ol,
+  ul {
     margin-top: 0;
     margin-bottom: 10px;
-}
-* {
+  }
+
+  * {
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     box-sizing: border-box;
-}
-user agent stylesheet
-ul {
+  }
+
+  user agent stylesheet ul {
     display: block;
     list-style-type: disc;
     margin-block-start: 1em;
@@ -67,28 +73,27 @@ ul {
     margin-inline-start: 0px;
     margin-inline-end: 0px;
     padding-inline-start: 40px;
-}
-
-
+  }
 </style>
+
 <body>
   <!-- side bar  -->
-                  <div class="sidebar">
-                    <div class="logo-details">
-                      <!-- <i class='bx bxl-c-plus-plus'></i> -->
-                      <i><img src="../images/logo/pngaaa.com-5968111.png" style="width: 70px;"></i>
-                      <span class="logo_name" style="width: 10px;">Stock Management System</span>
+  <div class="sidebar">
+    <div class="logo-details">
+      <!-- <i class='bx bxl-c-plus-plus'></i> -->
+      <i><img src="../images/logo/pngaaa.com-5968111.png" style="width: 70px;"></i>
+      <span class="logo_name" style="width: 10px;">Stock Management System</span>
 
-                    </div>
+    </div>
 
-                      <ul class="nav-links">
-                        <li>
-                          <a href="admin_Dashboard.php" class="active">
-                            <i class='bx bx-grid-alt' ></i>
-                            <span class="links_name">Dashboard</span>
-                          </a>
-                        </li>
-                       <!--  <li>
+    <ul class="nav-links">
+      <li>
+        <a href="admin_Dashboard.php" class="active">
+          <i class='bx bx-grid-alt'></i>
+          <span class="links_name">Dashboard</span>
+        </a>
+      </li>
+      <!--  <li>
                           <a href="admin_Image.php" class="active">
                             <i class='bx bx-image-alt' ></i>
                             <span class="links_name">MONITOR</span>
@@ -119,49 +124,48 @@ ul {
                             <span class="links_name">ALL in ONE</span>
                           </a>
                         </li> -->
-                         <?php 
-                                       $query=mysqli_query($con," SELECT * FROM `product` ORDER BY `id` DESC")or die(mysqli_error($con));
-                                                         // echo 1;die;
-                                                         while($row=mysqli_fetch_array($query)){
-                                       ?>
-                                        
-                        <li>
-                          <a href="<?php echo $row['url'] ?>" class="active">
-                            <i class='bx bx-donate-heart' ></i>
-                            <span class="links_name"><?php echo $row['NAME'] ?></span>
-                          </a>
-                        </li>
-                                         <?php
-                                       }
-                                       ?>  
-                         <li>
-                          <a href="admin_USER.php" class="active">
-                            <i class='bx bx-image-alt' ></i>
-                            <span class="links_name">USER</span>
-                          </a>
-                        </li>
-                        
-                        <li>
-                          <a href="admin_VENDOR.php" class="active">
-                            <i class='bx bx-grid-alt' ></i>
-                            <span class="links_name">VENDOR</span>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="admin_AMC.php" class="active">
-                            <i class='bx bx-donate-heart' ></i>
-                            <span class="links_name">AMC</span>
-                          </a>
-                        </li>
-                       
-                        
-                        <li class="log_out">
-                          <a href="logout.php">
-                            <i class='bx bx-log-out'></i>
-                            <span class="links_name">Log out</span>
-                          </a>
-                        </li>
-                      </ul>
-                  </div>
+      <?php
+      $query = mysqli_query($con, " SELECT * FROM `product` ORDER BY `id` DESC") or die(mysqli_error($con));
+      // echo 1;die;
+      while ($row = mysqli_fetch_array($query)) {
+      ?>
+
+        <li>
+          <a href="<?php echo $row['url'] ?>" class="active">
+            <i class='bx bx-donate-heart'></i>
+            <span class="links_name"><?php echo $row['NAME'] ?></span>
+          </a>
+        </li>
+      <?php
+      }
+      ?>
+      <li>
+        <a href="admin_USER.php" class="active">
+          <i class='bx bx-image-alt'></i>
+          <span class="links_name">USER</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="admin_VENDOR.php" class="active">
+          <i class='bx bx-grid-alt'></i>
+          <span class="links_name">VENDOR</span>
+        </a>
+      </li>
+      <li>
+        <a href="admin_AMC.php" class="active">
+          <i class='bx bx-donate-heart'></i>
+          <span class="links_name">AMC</span>
+        </a>
+      </li>
+
+
+      <li class="log_out">
+        <a href="logout.php">
+          <i class='bx bx-log-out'></i>
+          <span class="links_name">Log out</span>
+        </a>
+      </li>
+    </ul>
+  </div>
   <!-- end side bars -->
- 
